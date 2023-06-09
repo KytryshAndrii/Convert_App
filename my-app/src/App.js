1eca43afda2c264e367f532934cd5c79ffc55eb8
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-06-09 17:21:53 +0200

    First try with fs module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { writeFile } from 'fs/promises';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
 
 function App() {
+
+  function convertFunction(e){
+    const files = Array.from(e.target.files)
+    console.log("files:", files)
+  }
+
   return (
     <div className="App">
+
       <p>FFMPEG is COOL!</p>
+      <input type="file" id="files" name="files" accept="video/mov" onChange={convertFunction} multiple/>
+
     </div>
   );
 }
