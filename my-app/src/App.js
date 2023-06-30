325c901fdabed332a0a3f38a41fcd422bd79f556
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-06-30 20:19:30 +0200

    now it is another error message, FS is working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,50 @@
-import {React, useState} from "react"; 
+import {React, useEffect, useState} from "react"; 
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
+const ffmpeg = createFFmpeg({log: true});
 
 
 function App() {
 
-  const [videoFiles, setVideoFiles] = useState({})
+  const [videoFiles, setVideoFiles] = useState({});
 
-  function convertFunction(e){
-    const files = Array.from(e.target.files)
-    console.log("files:", files)
-    setVideoFiles(files)
+  const [readyVideos, setReadyVideos] = useState();
+
+  const [ready, setReady] = useState(false);
+
+  async function load (){
+    await ffmpeg.load();
+    setReady(true);
   }
 
-  return (
-    <div className="App">
+  useEffect(()=>{
+    load();
+  }, [])
 
+  async function convertFunction(e){
+    const files = Array.from(e.target.files[0])
+    console.log("files:", files)
+    setVideoFiles(files)
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(videoFiles));
+    await ffmpeg.run("-i", 'test.mp4', '-vcodec', 'copy', '-acodec', 'copy', 'out.mp4');
+    const data = ffmpeg.FS("readFile", "out.mp4");
+
+    const url = URL.createObjectURL(new Blob(data.buffer), {type: 'video/mp4'});
+    setReadyVideos(url);
+  }
+
+  return ready ? (
+    <div className="App">
+      {videoFiles?<p>hi</p>:<p>huj</p>}
       <p>FFMPEG is COOL!</p>
       <input type="file" id="files" name="files" accept="video/mov" onChange={convertFunction} multiple/>
-
+      <video controls 
+              width='250'
+              src={readyVideos}
+      
+      ></video>
     </div>
-  );
+  ) : (<p>Loading .....</p>)
 }
 
 export default App;
